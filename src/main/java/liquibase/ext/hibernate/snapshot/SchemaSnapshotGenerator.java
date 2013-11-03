9129445a51b15dbbac4c1b354ee6bf618b0e50e3
Jan Marten <jan.marten@gmx.de>
2013-11-03 12:28:30 +0100

    Set the defaultCatalogName and defaultSchemaName once and then use the AbstractJdbcDatabase getter/setter Otherwise the names cannot be customized e.g. in tests

--- src/main/java/liquibase/ext/hibernate/snapshot/SchemaSnapshotGenerator.java
+++ src/main/java/liquibase/ext/hibernate/snapshot/SchemaSnapshotGenerator.java
@@ -18,7 +18,7 @@ public class SchemaSnapshotGenerator extends HibernateSnapshotGenerator {
 
     @Override
     protected DatabaseObject snapshotObject(DatabaseObject example, DatabaseSnapshot snapshot) throws DatabaseException, InvalidExampleException {
-        return new Schema("HIBERNATE", "HIBERNATE");
+        return new Schema(snapshot.getDatabase().getDefaultCatalogName(), snapshot.getDatabase().getDefaultSchemaName());
     }
 
     @Override
