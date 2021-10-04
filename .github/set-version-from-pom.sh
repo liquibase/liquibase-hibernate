9bcca079f14d3e35099e98a47856c7b4d96881be
Nathan Voxland <nathan@voxland.net>
2021-10-04 13:16:11 -0500

    Updated release automation - Use triggers vs. dependabot

--- .github/set-version-from-pom.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-set -o pipefail
-
-echo "VERSION_TAG=$(mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.version -q -DforceStdout)" >> $GITHUB_ENV
