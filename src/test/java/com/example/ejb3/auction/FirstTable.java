de53805390aa7cd2637d2c0b1f5aa8efa226742e
Anton <anton.barkan@dig.security>
2023-07-01 14:54:54 +0300

    Support SecondaryTable annotation

---
+++ src/test/java/com/example/ejb3/auction/FirstTable.java
@@ -0,0 +1,42 @@
+package com.example.ejb3.auction;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@SecondaryTable(name = "second_table", pkJoinColumns = @PrimaryKeyJoinColumn(name = "first_table_id"))
+public class FirstTable {
+    @Id
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Embedded
+    private SecondTable secondTable;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public SecondTable getSecondTable() {
+        return secondTable;
+    }
+
+    public void setSecondTable(SecondTable secondTable) {
+        this.secondTable = secondTable;
+    }
+}
