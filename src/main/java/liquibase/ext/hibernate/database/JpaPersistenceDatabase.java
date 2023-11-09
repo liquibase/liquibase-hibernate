6b1e55be46ce5a5015095e795c4ce3ebd122a202
HARPER Jon <jon.harper87@gmail.com>
2023-11-09 11:44:23 +0100

    use hibernate.temp.use_jdbc_metadata_defaults=false everywhere like for HibernateClassic

--- src/main/java/liquibase/ext/hibernate/database/JpaPersistenceDatabase.java
+++ src/main/java/liquibase/ext/hibernate/database/JpaPersistenceDatabase.java
@@ -1,6 +1,6 @@
 package liquibase.ext.hibernate.database;
 
-import java.util.Collections;
+import java.util.Map;
 
 import org.hibernate.jpa.boot.internal.EntityManagerFactoryBuilderImpl;
 import org.hibernate.jpa.boot.spi.Bootstrap;
@@ -50,7 +50,8 @@ public class JpaPersistenceDatabase extends HibernateEjb3Database {
         internalPersistenceUnitManager.preparePersistenceUnitInfos();
         PersistenceUnitInfo persistenceUnitInfo = internalPersistenceUnitManager.obtainDefaultPersistenceUnitInfo();
 
-        EntityManagerFactoryBuilderImpl builder = (EntityManagerFactoryBuilderImpl) Bootstrap.getEntityManagerFactoryBuilder(persistenceUnitInfo, Collections.emptyMap());
+        EntityManagerFactoryBuilderImpl builder = (EntityManagerFactoryBuilderImpl) Bootstrap.getEntityManagerFactoryBuilder(persistenceUnitInfo, Map.of(
+                HibernateDatabase.HIBERNATE_TEMP_USE_JDBC_METADATA_DEFAULTS, Boolean.FALSE.toString()));
         return builder;
     }
 
