bd4c6e03ce7efec1743cf02dbc74e2789b2f762f
Filipe Lautert <flautert@liquibase.org>
2026-03-02 10:13:44 -0300

    Fix phantom diffs for PKs, unique constraints, and indexes on PostgreSQL
    Suppress the "using" attribute difference (btree vs null) in PK and UC
    change generators, and add a new ChangedIndexChangeGenerator to suppress
    "unique" and "using" differences on indexes.
    
    Fixes #718, #436, #456

--- src/main/java/liquibase/ext/hibernate/diff/ChangedIndexChangeGenerator.java
+++ src/main/java/liquibase/ext/hibernate/diff/ChangedIndexChangeGenerator.java
@@ -7,17 +7,17 @@ import liquibase.diff.output.DiffOutputControl;
 import liquibase.diff.output.changelog.ChangeGeneratorChain;
 import liquibase.ext.hibernate.database.HibernateDatabase;
 import liquibase.structure.DatabaseObject;
-import liquibase.structure.core.PrimaryKey;
+import liquibase.structure.core.Index;
 
 /**
- * Hibernate doesn't know about all the variations that occur with primary keys, especially backing index stuff.
- * To prevent changing customized primary keys, we suppress this kind of changes from hibernate side.
+ * Hibernate doesn't know about all index attributes (unique, using), so we suppress
+ * false differences to prevent unnecessary drop/recreate of indexes.
  */
-public class ChangedPrimaryKeyChangeGenerator extends liquibase.diff.output.changelog.core.ChangedPrimaryKeyChangeGenerator {
+public class ChangedIndexChangeGenerator extends liquibase.diff.output.changelog.core.ChangedIndexChangeGenerator {
 
     @Override
     public int getPriority(Class<? extends DatabaseObject> objectType, Database database) {
-        if (PrimaryKey.class.isAssignableFrom(objectType)) {
+        if (Index.class.isAssignableFrom(objectType)) {
             return PRIORITY_ADDITIONAL;
         }
         return PRIORITY_NONE;
@@ -27,13 +27,11 @@ public class ChangedPrimaryKeyChangeGenerator extends liquibase.diff.output.chan
     public Change[] fixChanged(DatabaseObject changedObject, ObjectDifferences differences, DiffOutputControl control, Database referenceDatabase, Database comparisonDatabase, ChangeGeneratorChain chain) {
         if (referenceDatabase instanceof HibernateDatabase || comparisonDatabase instanceof HibernateDatabase) {
             differences.removeDifference("unique");
-            differences.removeDifference("validate");
             differences.removeDifference("using");
             if (!differences.hasDifferences()) {
                 return null;
             }
         }
-
         return super.fixChanged(changedObject, differences, control, referenceDatabase, comparisonDatabase, chain);
     }
 }
