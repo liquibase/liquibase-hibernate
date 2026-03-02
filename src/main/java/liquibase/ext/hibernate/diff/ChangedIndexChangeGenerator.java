4ba697cffae21fb204758183d68f37b8e732674e
Filipe Lautert <flautert@liquibase.org>
2026-03-02 11:41:54 -0300

    Add ChangedIndexChangeGenerator to suppress phantom index diffs
    Hibernate doesn't populate the "unique" and "using" attributes on
    indexes, causing false drop/recreate changesets on every diff run.
    This complements the PK and UC fixes for a fully clean re-diff.

---
+++ src/main/java/liquibase/ext/hibernate/diff/ChangedIndexChangeGenerator.java
@@ -0,0 +1,37 @@
+package liquibase.ext.hibernate.diff;
+
+import liquibase.change.Change;
+import liquibase.database.Database;
+import liquibase.diff.ObjectDifferences;
+import liquibase.diff.output.DiffOutputControl;
+import liquibase.diff.output.changelog.ChangeGeneratorChain;
+import liquibase.ext.hibernate.database.HibernateDatabase;
+import liquibase.structure.DatabaseObject;
+import liquibase.structure.core.Index;
+
+/**
+ * Hibernate doesn't know about all index attributes (unique, using), so we suppress
+ * false differences to prevent unnecessary drop/recreate of indexes.
+ */
+public class ChangedIndexChangeGenerator extends liquibase.diff.output.changelog.core.ChangedIndexChangeGenerator {
+
+    @Override
+    public int getPriority(Class<? extends DatabaseObject> objectType, Database database) {
+        if (Index.class.isAssignableFrom(objectType)) {
+            return PRIORITY_ADDITIONAL;
+        }
+        return PRIORITY_NONE;
+    }
+
+    @Override
+    public Change[] fixChanged(DatabaseObject changedObject, ObjectDifferences differences, DiffOutputControl control, Database referenceDatabase, Database comparisonDatabase, ChangeGeneratorChain chain) {
+        if (referenceDatabase instanceof HibernateDatabase || comparisonDatabase instanceof HibernateDatabase) {
+            differences.removeDifference("unique");
+            differences.removeDifference("using");
+            if (!differences.hasDifferences()) {
+                return null;
+            }
+        }
+        return super.fixChanged(changedObject, differences, control, referenceDatabase, comparisonDatabase, chain);
+    }
+}
